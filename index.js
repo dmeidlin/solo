45a6a0be308d8514e9f9b1366a55948c679b199e
dmeidlin <git_hub@nym.hush.com>
2016-01-05 00:17:23 -0500

    Call listens(server) when socket.io is loaded.

--- index.js
+++ index.js
@@ -2,12 +2,15 @@
 // var http = require('http').Server(app);
 // var io = require('socket.io')(http);
 
-var express = require('express');
-var app = express();
-var http = require('http');
-var server = http.createServer(app);
-var ioSocket = require('socket.io');
-var io = ioSocket(server);
+var express = require('express'),
+    app = express(),
+    server = require('http').createServer(app),
+    io = require('socket.io').listen(server),
+     /* listen to the port set by the service provider;
+      otherwise 3000 by default: */
+    port = process.env.PORT || 3000;
+    
+
 app.get('/', function (req, res) {
   res.sendFile(__dirname + '/index.html');
 });
@@ -25,10 +28,6 @@ io.on('connection', function(socket){
     io.emit('chat message', msg);
   });
 });
-
-/* listen to the port set by the service provider;
- otherwise 3000 by default: */
-var port = process.env.PORT || 3000;
 
 //http.listen(...
 server.listen(port, function () {
